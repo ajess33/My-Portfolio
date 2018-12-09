eaa377a4f82f18be4a58ffe641d04b0311cdee40
Austin Jess <ajess33@gmail.com>
2018-12-09 00:16:28 -0600

    added portfolio pics

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -13,11 +13,11 @@ const Portfolio = () => (
           THE <br /> PORTFOLIO
         </h3>
       </div>
-      <p className="desc-main">
+      <p className="desc-main spacer">
         Here are a few projects I have made while teaching myself web
         development
       </p>
-      <div className="project--list">
+      <div className="projects-list">
         <ProjIpsum />
       </div>
       <div className="div__center">
@@ -28,25 +28,36 @@ const Portfolio = () => (
 )
 
 const ProjIpsum = () => (
-  <div className="project--wrapper">
+  <div className="project">
     <h4>DUNDER-MIFFLINIPSUM</h4>
-    <div className="project--desc">
-      <p>
-        This is a lorem ipsum generator using quotes from the tv show, 'The
-        Office'. I created this while I was learning React.js. This was the
-        first project I had made using React and it helped solidify some of what
-        I had learned in tutorials. One challenge I had was working with state
-        and getting the data to the correct component. After much frustration I
-        was able to solve the problem by rethinking the state. Despite the
-        challenges, this was the project I fell in love with React!
-      </p>
-      <ProjectLinks />
+    <div className="project--flex">
+      <img src={DunderScreenshot} alt="Dunder-MifflinIpsum screenshot" />
+      <div>
+        <p className="project--desc">
+          This is a lorem ipsum generator using quotes from the tv show, 'The
+          Office'. I created this while I was learning React.js. Because of
+          this, I didn't spend a whole lot of time styling. This was the first
+          project I had made using React and it helped solidify some of what I
+          had learned in tutorials. One challenge I had was working with state
+          and getting the data to the correct component. After much frustration
+          I was able to solve the problem by rethinking the state. Despite the
+          challenges, this was the project I began to fell in love with React!
+        </p>
+        <ProjectLinks />
+      </div>
+      <div className="project__seperator" />
     </div>
   </div>
 )
 
+const ProjACG = () => (
+  <div className="project--wrapper">
+    <h4>A CLEAN GETAWAY WEBSITE</h4>
+  </div>
+)
+
 const ProjectLinks = () => (
-  <div>
+  <div className="buttons-flex">
     <a className="link" to="https://ajess33.github.io/dunder-mifflinipsum/">
       SEE IT LIVE
     </a>
@@ -57,34 +68,3 @@ const ProjectLinks = () => (
 )
 
 export default Portfolio
-
-// const Dunder = () => (
-//   <div className="project">
-//     <h4 className="project--title">DUNDER-MIFFLINIPSUM</h4>
-//     <div className="flex-wrap">
-//       <div>
-//         <p>
-//         </p>
-//       </div>
-//       <div>
-//         <img src={DunderScreenshot} alt="dunder-mifflinipsum screenshot" />
-//         <ProjectLinks />
-//       </div>
-//     </div>
-//   </div>
-// )
-
-// const CleanWebsite = () => (
-//   <div className="project">
-//     <h4 className="project--title">BUSINESS WEBSITE</h4>
-//     <div className="flex-wrap">
-//       <h5>Redesign of A Clean Getaway, LLC business website</h5>
-//     </div>
-//   </div>
-// )
-
-// const PomoClock = () => (
-//   <div className="project">
-//     <h4 className="project--title">POMODORO CLOCK</h4>
-//   </div>
-// )
